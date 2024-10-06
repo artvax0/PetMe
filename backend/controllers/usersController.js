e3848ea206ed848feb1c621253a99aae0efd89ba
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-10-06 12:47:12 +0300

    add starting login endpoint

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,12 +1,22 @@
 import chalk from "chalk";
 import { Router } from "express";
-import { registerUser } from "../services/usersService.js";
+import { login, registerUser } from "../services/usersService.js";
 
 const router = Router();
 
 router.post('/', async (req, res) => {
   try {
     const user = await registerUser(req.body);
+    res.send(user);
+  } catch (error) {
+    throw new Error(chalk.red(error.message));
+  }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    const user = await login(email, password);
     res.send(user);
   } catch (error) {
     throw new Error(chalk.red(error.message));
--- backend/services/usersService.js
+++ backend/services/usersService.js
@@ -2,6 +2,7 @@ import chalk from "chalk";
 import User from "../models/collections/Users.js"
 import _ from 'lodash'
 
+// [POST]
 const registerUser = async (newUser) => {
   try {
     let user = new User(newUser);
@@ -12,4 +13,17 @@ const registerUser = async (newUser) => {
   }
 }
 
-export { registerUser };
+const login = async (email, password) => {
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user || user.password != password) {
+      throw new Error(chalk.red('Invalid email or password'));
+    }
+    return `user logged in: ${user._id}`;
+  } catch (error) {
+    throw new Error(chalk.red('Moongoose: ', error));
+  }
+}
+
+export { registerUser, login };
