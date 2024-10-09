1818e38b67206c41e29a95b7cf9f4f4a2e1e78f0
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-10-09 18:22:01 +0300

    remove console.log

--- backend/utils/handleErrors.js
+++ backend/utils/handleErrors.js
@@ -1,7 +1,6 @@
 import chalk from "chalk";
 
 const createError = (validator, error) => {
-  console.log(chalk.yellow(validator))
   error.message = `${validator} Error: ${error.message}`;
   error.status = error.status || 400;
   throw error;
