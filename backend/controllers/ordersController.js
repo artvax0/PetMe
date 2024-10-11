6f54526586d2e901600a861a2e02c0a429d5aa2e
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-10-11 13:43:04 +0300

    add post endpoint - create new order

--- backend/controllers/ordersController.js
+++ backend/controllers/ordersController.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import { newOrder } from "../services/ordersService.js";
+import { handleError } from "../utils/handleErrors.js";
 
 const router = Router();
+
+router.post('/', async (req, res) => {
+  try {
+    let order = await newOrder(req.body);
+    res.send(order);
+  } catch (error) {
+    handleError(res, error);
+  }
+})
 
 export default router;
---
+++ backend/services/ordersService.js
@@ -0,0 +1,24 @@
+import Order from "../models/collections/Order.js"
+import { createError } from "../utils/handleErrors.js";
+
+const newOrder = async (orderDetails) => {
+  try {
+    let order = new Order(orderDetails);
+    order.total ??= 0;
+    order.products.forEach(product => order.total += product.price);
+
+    // if user provides status to a new order - that isnt processing, reject request
+    if (order.status != 'Processing') {
+      let error = Error('Invalid Status');
+      error.status = 400;
+      return createError('Bad Request', error);
+    }
+
+    order = await order.save();
+    return order;
+  } catch (error) {
+    return createError('Mongoose', error);
+  }
+}
+
+export { newOrder };
