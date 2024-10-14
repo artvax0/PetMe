6e346319873f33684a4e2d6071c7cfde1d16008f
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-10-15 00:55:56 +0300

    add cartsController authorization

--- backend/controllers/cartsController.js
+++ backend/controllers/cartsController.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { handleError } from "../utils/handleErrors.js";
 import { addToCart, createCart, getCart } from "../services/cartsService.js";
+import authLoggedUser from "../middlewares/userAuth.js";
 
 const router = Router();
 
-router.post('/:id', async (req, res) => {
+router.post('/:id', authLoggedUser, async (req, res) => {
   try {
+    const payload = res.locals.user;
+    if (!payload.isAdmin) {
+      let error = Error('Only system admins can add a cart');
+      error.status = 405;
+      error.validator = 'Authorization';
+      return handleError(res, error);
+    }
     const { id } = req.params;
     let cart = await createCart(id);
     res.send(cart);
@@ -14,9 +22,16 @@ router.post('/:id', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', authLoggedUser, async (req, res) => {
   try {
     const { id } = req.params;
+    const payload = res.locals.user;
+    if (!payload.isAdmin || payload._id != id) {
+      let error = Error('Users can only view their own cart');
+      error.status = 405;
+      error.validator = 'Authorization';
+      return handleError(res, error);
+    }
     let cart = await getCart(id);
     res.send(cart);
   } catch (error) {
@@ -24,9 +39,16 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', authLoggedUser, async (req, res) => {
   try {
     const { id } = req.params;
+    const payload = res.locals.user;
+    if (!payload.isAdmin || payload._id != id) {
+      let error = Error('Users can only add products to their own cart');
+      error.status = 405;
+      error.validator = 'Authorization';
+      return handleError(res, error);
+    }
     let cart = await addToCart(id, req.body);
     res.send(cart);
   } catch (error) {
@@ -35,7 +57,7 @@ router.patch('/:id', async (req, res) => {
 });
 
 // for when user attempts to delete a cart
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', authLoggedUser, async (req, res) => {
   try {
     res.status(405).send('Not Allowed: Deleting carts not permitted');
   } catch (error) {
