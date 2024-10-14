f88f7f5eea51de1fcf5eaaa694a037b1fde4f2fa
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-10-15 00:46:39 +0300

    add orderController authorization

--- backend/controllers/ordersController.js
+++ backend/controllers/ordersController.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
 import { changeOrderStatus, getOrder, getOrders, getOrdersFromUser, newOrder } from "../services/ordersService.js";
 import { createError, handleError } from "../utils/handleErrors.js";
+import authLoggedUser from "../middlewares/userAuth.js";
 
 const router = Router();
 
-router.post('/', async (req, res) => {
+router.post('/:id', authLoggedUser, async (req, res) => {
   try {
+    const { id } = req.params;
+    const payload = res.locals.user;
+    if (payload._id != id) {
+      let error = Error('Cannot add an order for another user');
+      error.status = 405;
+      error.validator = 'Authorization';
+      return handleError(res, error);
+    }
     let order = await newOrder(req.body);
     res.send(order);
   } catch (error) {
@@ -13,8 +22,15 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.get('/', async (req, res) => {
+router.get('/', authLoggedUser, async (req, res) => {
   try {
+    const payload = res.locals.user;
+    if (!payload.isAdmin) {
+      let error = Error('Only admins can view all orders');
+      error.status = 405;
+      error.validator = 'Authorization';
+      return handleError(res, error);
+    }
     let orders = await getOrders();
     res.send(orders);
   } catch (error) {
@@ -25,6 +41,13 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
+    const payload = res.locals.user;
+    if (!payload.isAdmin || payload._id != id) {
+      let error = Error('Cannot view an other user order');
+      error.status = 405;
+      error.validator = 'Authorization';
+      return handleError(res, error);
+    }
     let order = await getOrder(id);
     res.send(order);
   } catch (error) {
@@ -35,6 +58,13 @@ router.get('/:id', async (req, res) => {
 router.get('/user/:id', async (req, res) => {
   try {
     const { id } = req.params;
+    const payload = res.locals.user;
+    if (!payload.isAdmin || payload._id != id) {
+      let error = Error('Cannot view an other user orders');
+      error.status = 405;
+      error.validator = 'Authorization';
+      return handleError(res, error);
+    }
     let orders = await getOrdersFromUser(id);
     res.send(orders);
   } catch (error) {
@@ -44,6 +74,13 @@ router.get('/user/:id', async (req, res) => {
 
 router.patch('/:id', async (req, res) => {
   try {
+    const payload = res.locals.user;
+    if (!payload.isAdmin || !payload.isEmployee) {
+      let error = Error('Only employees can change an order status');
+      error.status = 405;
+      error.validator = 'Authorization';
+      return handleError(res, error);
+    }
     const { id } = req.params;
     let order = await changeOrderStatus(id, req.body);
     res.send(order);
