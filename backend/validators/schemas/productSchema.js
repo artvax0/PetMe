2db40ea3ab4a65da17b76878ac42a9f8f31255a9
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-10-20 16:44:30 +0300

    add product joi schema

---
+++ backend/validators/schemas/productSchema.js
@@ -0,0 +1,21 @@
+import Joi from "joi";
+
+const productSchema = (product) => {
+  const schema = Joi.object({
+    name: Joi.string().min(2).max(256).required(),
+    description: Joi.string().min(2).max(256).required(),
+    image: Joi.object().keys({
+      url: Joi.string()
+        .ruleset.regex(/(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/)
+        .rule({ message: 'User avatar must be a valid URL' })
+        .allow(''),
+      alt: Joi.string().min(0).max(256).allow('')
+    }).required(),
+    price: Joi.number().min(0).required(),
+    stock: Joi.number().min(0).required(),
+    category_id: Joi.string().required()
+  });
+  return schema.validate(product);
+}
+
+export default productSchema;
