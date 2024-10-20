4d4ff17b7f86f9089513a04b5033db9f8c2bee8c
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-10-20 18:16:46 +0300

    update config error to switch-case

--- backend/utils/configError.js
+++ backend/utils/configError.js
@@ -1,16 +1,26 @@
 import { createError } from "./handleErrors.js";
 
 const configError = (errorType) => {
-  if (errorType == 'db') {
-    let error = Error('There is no database for this request');
-    error.status = 500;
-    return createError('Database', error);
-  }
+  let error;
+  switch (errorType) {
+    case 'db':
+      error = Error('There is no database for this request');
+      error.status = 500;
+      return createError('Database', error);
 
-  if (errorType == 'tokenGen') {
-    let error = Error('There is no valid token generator.');
-    error.status = 500;
-    return createError('Token Generator', error);
+    case 'tokenGen':
+      error = Error('There is no token generator for this request');
+      error.status = 500;
+      return createError('Token Generator', error);
+
+
+    case 'validator':
+      error = Error('There is no validator for this request');
+      error.status = 500;
+      return createError('Validator', error);
+
+    default:
+      break;
   }
 }
 
