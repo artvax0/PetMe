0c9856afa9cf8bb496616f47ca599d26457a351f
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-11-03 18:19:18 +0200

    update product visuals

--- frontend/src/components/cards/CardComponent.jsx
+++ frontend/src/components/cards/CardComponent.jsx
@@ -1,30 +1,37 @@
-import { Button, Card, CardActionArea, CardContent, Typography } from "@mui/material"
+import { Box, Button, Card, CardActionArea, CardContent, Typography } from "@mui/material"
 import CardImage from "./CardImage"
 import { useTheme } from "../../providers/ThemeProvider"
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../routes/routesModel";
 
 export default function CardComponent({ product }) {
   const { theme } = useTheme();
+  const navigate = useNavigate();
   return (
     <>
-      <Card sx={{ width: '100%', backgroundColor: theme.palette.primary.main, }}>
-        <CardActionArea sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <CardImage url={product.image.url} alt={product.image.alt} />
+      <Card elevation={5} sx={{ width: '100%', backgroundColor: theme.palette.primary.main, borderRadius: '12px' }}>
+        <CardActionArea onClick={() => navigate(ROUTES.PRODUCT + `/${product._id}`)} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Box height='50%' width='100%'>
+            <CardImage url={product.image.url} alt={product.image.alt} />
+          </Box>
 
-          <CardContent>
-            <Typography gutterBottom variant="body1" fontWeight={500} component='p' sx={{ textAlign: 'center' }}>{product.name}</Typography>
-            <Typography variant='h6' fontWeight='medium' component='p' sx={{ textAlign: 'center' }}>${product.price}</Typography>
-          </CardContent>
+          <Box height='50%'>
+            <CardContent sx={{ p: 1 }}>
+              <Typography gutterBottom variant="body1" fontWeight={500} component='p' sx={{ textAlign: 'center' }}>{product.name}</Typography>
+              <Typography variant='h6' fontWeight='medium' component='p' sx={{ textAlign: 'center' }}>${product.price}</Typography>
+            </CardContent>
 
-          <Button component='div' variant="contained"
-            sx={{
-              backgroundColor: theme.palette.success.main,
-              boxShadow: `0 4px 0 0 ${theme.palette.success.dark}`,
-              // '&:hover': { backgroundColor: theme.palette.primary.dark },
-              mb: 1
-            }}
-          >
-            Purchase
-          </Button>
+            <Button component='div' variant="contained"
+              sx={{
+                backgroundColor: theme.palette.success.main,
+                boxShadow: `0 4px 0 0 ${theme.palette.success.dark}`,
+                // '&:hover': { backgroundColor: theme.palette.primary.dark },
+                mb: 2
+              }}
+            >
+              Purchase
+            </Button>
+          </Box>
         </CardActionArea>
       </Card >
     </>
--- frontend/src/components/cards/CardImage.jsx
+++ frontend/src/components/cards/CardImage.jsx
@@ -3,7 +3,7 @@ import { CardMedia } from "@mui/material";
 export default function CardImage({ url, alt }) {
   return (
     <CardMedia
-      sx={{ width: '100%', height: 175 }}
+      sx={{ aspectRatio: '1/1', margin: 1.5, backgroundColor: '#fff', borderRadius: '20px', boxShadow: 'inset 0 4px 4px rgba(0, 0, 0, .25), inset 0 -4px 4px rgba(0, 0, 0, .25)' }}
       image={url}
       title={alt}
     >
