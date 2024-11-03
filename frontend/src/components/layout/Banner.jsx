b94d2fd270b5d78332b742668e8a31f5fc7af765
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-11-03 16:54:13 +0200

    animate banner

--- frontend/src/components/layout/Banner.jsx
+++ frontend/src/components/layout/Banner.jsx
@@ -4,7 +4,11 @@ import React from 'react'
 export default function Banner() {
   return (
     <Box sx={{ backgroundColor: 'gold', height: 7 }}>
-      <Box mx={75} sx={{ background: 'linear-gradient(90deg, rgb(255, 255, 255) 0%, rgb(255, 255, 255) 35%, rgb(55, 108, 255) 35%, rgb(55, 108, 255) 65%, rgb(255, 255, 255) 65%, rgb(255, 255, 255) 100%)', height: '100%' }} />
+      <Box mx={75} sx={{
+        background: 'linear-gradient(90deg, rgb(255, 255, 255) 0%, rgb(255, 255, 255) 35%, rgb(55, 108, 255) 35%, rgb(55, 108, 255) 65%, rgb(255, 255, 255) 65%, rgb(255, 255, 255) 100%)', height: '100%',
+        '&:hover': { mx: 50 },
+        transition: 'all 2s'
+      }} />
     </Box>
   )
 }
