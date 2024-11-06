9fe6c8a75fc498b23cf07275b246895f3054a44e
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-11-06 17:56:08 +0200

    add boxShadow to textfield to match other buttons

--- frontend/src/components/utils/Counter.jsx
+++ frontend/src/components/utils/Counter.jsx
@@ -26,7 +26,7 @@ export default function Counter() {
   return (
     <>
       <Button variant='contained' aria-label='Lower Quantity' onClick={lowerCount} sx={{ p: 0, minWidth: '30px', aspectRatio: '1/1' }}>-</Button>
-      <TextField value={count} aria-label='Quantity' defaultValue={1} type='number' onChange={(e) => handleValueChange(e)} slotProps={{ htmlInput: { min: 1, max: 100, style: { padding: 0, minWidth: '30px', minHeight: '30px', aspectRatio: '1/1' } }, }} sx={{ p: 0 }} />
+      <TextField value={count} aria-label='Quantity' defaultValue={1} type='number' onChange={(e) => handleValueChange(e)} slotProps={{ htmlInput: { min: 1, max: 100, style: { padding: 0, minWidth: '30px', minHeight: '30px', aspectRatio: '1/1' } }, input: { style: { boxShadow: 'rgba(0, 0, 0, 0.2) 0px 3px 1px -2px, rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px' } } }} />
       <Button variant='contained' aria-label='Raise Quantity' onClick={raiseCount} sx={{ p: 0, minWidth: '30px', aspectRatio: '1/1' }}>+</Button>
     </>
   )
