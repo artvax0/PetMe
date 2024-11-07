b5da07bbf1ee8676b32667fee4b4b717819345f1
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-11-07 23:17:15 +0200

    add title to products page

--- frontend/src/pages/ProductsPage.jsx
+++ frontend/src/pages/ProductsPage.jsx
@@ -3,6 +3,7 @@ import CardComponent from "../components/cards/CardComponent";
 import { useEffect } from "react";
 import useProducts from "../hooks/useProducts";
 import { useTheme } from "../providers/ThemeProvider";
+import Title from "../components/utils/Title";
 
 export default function ProductsPage() {
   const { theme } = useTheme();
@@ -15,25 +16,27 @@ export default function ProductsPage() {
   if (isLoading) return <p>Loading...</p> //temporarry loading message until loading spinner is ready
   if (error) return <p>Error: {error}</p> //temporary error message until error component is ready
   if (categories) return (
-    <Box width='100%' display='flex' flexDirection='column' gap={2}>
-      {categories.map(category => {
-        const products = productsByCategory[category._id] || [];
+    <>
+      <Title title={'Products'} />
+      <Box width='100%' display='flex' flexDirection='column' gap={2}>
+        {categories.map(category => {
+          const products = productsByCategory[category._id] || [];
 
-        if (products && products.length > 0) return (
-          <Box key={category._id} display='flex' flexDirection='column' gap={1}>
-            <Typography variant='h5' component='h2' fontWeight={theme.typography.fontWeightMedium}>{category.name}</Typography>
-            <Grid2 container spacing={2} mx={3}>
-              {products.map((product) => (
-                <Grid2 key={product._id} display='inline-flex' size={{ xs: 12, sm: 6, md: 4, lg: 1.5 }}>
-                  <CardComponent product={product} />
-                </Grid2>
-              ))}
-            </Grid2>
-          </Box>
-        )
-      })}
-    </Box>
-
+          if (products && products.length > 0) return (
+            <Box key={category._id} display='flex' flexDirection='column' gap={1}>
+              <Typography variant='h5' component='h2' fontWeight={theme.typography.fontWeightMedium}>{category.name}</Typography>
+              <Grid2 container spacing={2} mx={3}>
+                {products.map((product) => (
+                  <Grid2 key={product._id} display='inline-flex' size={{ xs: 12, sm: 6, md: 4, lg: 1.5 }}>
+                    <CardComponent product={product} />
+                  </Grid2>
+                ))}
+              </Grid2>
+            </Box>
+          )
+        })}
+      </Box>
+    </>
   )
 
   return null;
