6dd1d5413aa66e0ddd8a843321cda36b4b8d0119
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-11-14 14:18:16 +0200

    add account menu

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,5 +1,5 @@
-import { AppBar, Avatar, Box, Button, Drawer, Grid2, IconButton, styled, Toolbar } from '@mui/material'
-import { useEffect, useState } from 'react'
+import { AppBar, Avatar, Box, Button, Drawer, Grid2, IconButton, Menu, MenuItem, styled, Toolbar } from '@mui/material'
+import { useState } from 'react'
 import { useTheme } from '../../providers/ThemeProvider';
 import Banner from './Banner';
 import { ROUTES } from '../../routes/routesModel';
@@ -7,12 +7,27 @@ import { Link } from 'react-router-dom'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { useAuth } from '../../providers/UserProvider';
 import CartDrawer from '../cart/CartDrawer';
-import useCarts from '../../hooks/useCarts';
+import HistoryIcon from '@mui/icons-material/History';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { removeToken } from '../../services/storageService';
 
 export default function Header() {
   const { theme } = useTheme();
   const { user } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
+  const [menuAnchor, setMenuAnchor] = useState(null);
+  const isMenuOpen = Boolean(menuAnchor);
+  const handleMenu = (e) => {
+    setMenuAnchor(e.currentTarget);
+  }
+  const handleClose = () => {
+    setMenuAnchor(null);
+  }
+  const handleLogout = () => {
+    removeToken();
+    location.reload();
+  }
 
   const NavLink = styled(Link)(({ theme }) => `
     text-decoration: none;
@@ -41,7 +56,9 @@ export default function Header() {
               user ?
                 (<>
                   <IconButton onClick={() => setIsOpen(prev => !prev)}><ShoppingCartIcon sx={{ color: `${theme.palette.success.light}` }} /></IconButton>
-                  <Avatar />
+                  <IconButton onClick={handleMenu} size='small' aria-controls={isMenuOpen ? 'account-menu' : undefined} aria-haspopup='true' aria-expanded={open ? 'true' : undefined}>
+                    <Avatar />
+                  </IconButton>
                 </>)
                 : (<>
                   <Box display='inline-flex' gap={5}>
@@ -51,6 +68,47 @@ export default function Header() {
                 </>)
             }
             <CartDrawer isOpen={isOpen} setIsOpen={setIsOpen} />
+            <Menu
+              id='account-menu'
+              anchorEl={menuAnchor}
+              open={isMenuOpen}
+              onClose={handleClose}
+              onClick={handleClose}
+              slotProps={{
+                paper: {
+                  elevation: 0,
+                  sx: {
+                    overflow: 'visible',
+                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                    mt: 1.5,
+                    '& .MuiAvatar-root': {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    '&::before': {
+                      content: '""',
+                      display: 'block',
+                      position: 'absolute',
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: 'background.paper',
+                      transform: 'translateY(-50%) rotate(45deg)',
+                      zIndex: 0,
+                    },
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+              <MenuItem><HistoryIcon sx={{ mr: 1 }} />Orders History</MenuItem>
+              <MenuItem><SettingsIcon sx={{ mr: 1 }} />Account Preferences</MenuItem>
+              <MenuItem onClick={handleLogout}><LogoutIcon sx={{ mr: 1 }} />Logout</MenuItem>
+            </Menu>
           </Toolbar>
         </ AppBar>
       </Grid2>
