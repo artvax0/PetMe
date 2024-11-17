bb19b6db6b98cd056b0fe55eb03122335827e057
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-11-17 17:07:05 +0200

    add employee actions speed dial

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,11 +1,19 @@
-import { Box, Grid2 } from '@mui/material'
+import { Box, Grid2, SpeedDial, SpeedDialAction } from '@mui/material'
 import React from 'react'
 import Header from './Header'
 import { useTheme } from '../../providers/ThemeProvider'
 import Footer from './Footer';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { useAuth } from '../../providers/UserProvider';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../../routes/routesModel';
 
 export default function Layout({ children }) {
+  const { user } = useAuth();
   const { theme } = useTheme();
+  const navigate = useNavigate();
 
   return (
     <Box minHeight='100vh' display='flex' flexDirection='column' gap={0}>
@@ -14,6 +22,12 @@ export default function Layout({ children }) {
         <Grid2 container flexGrow={1} sx={{ backgroundColor: theme.palette.background.light, borderRadius: '30px', p: '8px 30px 30px 30px' }}>
           {children}
         </Grid2>
+        {user && user.isEmployee &&
+          <SpeedDial ariaLabel='User Actions' sx={{ position: 'fixed', bottom: 10, right: 10, '>button': { backgroundColor: `${theme.palette.highlight.main}` } }} icon={<ReceiptIcon />}>
+            <SpeedDialAction icon={<ShoppingBagIcon />} tooltipTitle='View Orders' sx={{ backgroundColor: `${theme.palette.highlight.main}` }} />
+            <SpeedDialAction onClick={() => navigate(ROUTES.ADD_PRODUCT)} icon={<AddShoppingCartIcon />} tooltipTitle='Create Product' sx={{ backgroundColor: `${theme.palette.highlight.main}` }} />
+          </SpeedDial>
+        }
       </Box>
       <Footer />
     </Box>
