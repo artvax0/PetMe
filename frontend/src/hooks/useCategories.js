53a8f1f1a07a26007027994366ca0d0dfe3a4511
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-11-19 01:05:51 +0200

    add getAllCategories endpoint

---
+++ frontend/src/hooks/useCategories.js
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { getCategories } from '../services/categoriesApiService';
+
+export default function useCategories() {
+  const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+
+  const getAllCategories = async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await getCategories();
+      setCategories(data);
+    } catch (error) {
+      setError(error);
+    }
+    setIsLoading(false);
+  }
+
+  return { categories, isLoading, error, getAllCategories };
+}
