2a89b0dcb844d028e3459d3cccce83b052c89e04
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-11-20 19:40:33 +0200

    add map product to model

---
+++ frontend/src/helpers/normalization/mapProductToModel.js
@@ -0,0 +1,17 @@
+import localTime from "../../utils/localTime";
+
+const mapProductToModel = (product) => ({
+  name: product.name,
+  description: product.name,
+  url: product.image.url,
+  alt: product.image.alt,
+  price: product.price,
+  stock: product.stock,
+  category_id: product.category_id,
+  petType_id: product.petType_id,
+  discount: product?.discount || '0',
+  discountStartDate: localTime(product?.discountStartDate) || localTime(Date.now()),
+  discountEndDate: localTime(product?.discountEndDate) || localTime(Date.now()),
+});
+
+export default mapProductToModel;
