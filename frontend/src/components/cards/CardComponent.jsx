a63a9fd8ebf73c60a86f97e5e16c9f9b7dccbca0
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-12-01 18:57:18 +0200

    add discount indicator on product card

--- frontend/src/components/cards/CardComponent.jsx
+++ frontend/src/components/cards/CardComponent.jsx
@@ -7,18 +7,26 @@ import { ROUTES } from "../../routes/routesModel";
 export default function CardComponent({ product, pets }) {
   const { theme } = useTheme();
   const navigate = useNavigate();
+  let isDiscountValid = false;
+  let now = new Date().toISOString();
+
+  if (product.discount > 0 && product.discountStartDate <= now && product.discountEndDate >= now) { isDiscountValid = true };
   return (
     <>
       <Card elevation={5} sx={{ width: '100%', backgroundColor: theme.palette.primary.main, borderRadius: '12px' }}>
         <CardActionArea onClick={() => navigate(ROUTES.PRODUCT + `/${product._id}`)} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <Box height='50%' width='100%'>
-            <CardImage url={product.image.url} alt={product.image.alt} pets={pets} petTypes={product.petType_id} />
+            <CardImage isDiscountValid={isDiscountValid} url={product.image.url} alt={product.image.alt} pets={pets} petTypes={product.petType_id} />
           </Box>
-
           <Box height='50%' display='flex' flexDirection='column' alignItems='center'>
             <CardContent sx={{ p: 1 }}>
               <Typography gutterBottom variant="h5" fontWeight={500} component='p' sx={{ textAlign: 'center' }}>{product.name}</Typography>
-              <Typography variant='h6' fontWeight='medium' component='p' sx={{ textAlign: 'center' }}>${product.price}</Typography>
+              {isDiscountValid ?
+                <Box display='flex' gap={1} justifyContent='center'>
+                  <Typography variant='h6' fontWeight='light' component='p' sx={{ textAlign: 'center', textDecoration: 'line-through' }} color='darkgrey' >${product.price}</Typography>
+                  <Typography variant='h6' fontWeight='medium' component='p' sx={{ textAlign: 'center' }} color='error'>${product.price * product.discount / 100}</Typography>
+                </Box> :
+                <Typography variant='h6' fontWeight='medium' component='p' sx={{ textAlign: 'center' }}>${product.price}</Typography>}
             </CardContent>
 
             <Button component='div' variant="contained"
--- frontend/src/components/cards/CardImage.jsx
+++ frontend/src/components/cards/CardImage.jsx
@@ -1,22 +1,30 @@
 import { Box, CardMedia } from "@mui/material";
 
-export default function CardImage({ url, alt, pets, petTypes }) {
+export default function CardImage({ isDiscountValid, url, alt, pets, petTypes }) {
   return (
     <CardMedia
       sx={{ aspectRatio: '1/1', margin: 1.5, backgroundColor: '#fff', borderRadius: '20px', boxShadow: 'inset 0 4px 4px rgba(0, 0, 0, .25), inset 0 -4px 4px rgba(0, 0, 0, .25)', position: 'relative' }}
       image={url}
       title={alt}
     >
-      {/* Pet Icons */}
-      <Box width='100%' display='inline-flex' flexDirection='row-reverse' gap={0.5} sx={{ position: 'absolute', bottom: 1, marginBottom: '-15px' }}>
-        {petTypes.map((petType) => {
-          const petInfo = pets.filter(pet => pet._id == petType)[0];
-          return (
-            <Box key={`${petInfo._id}-${alt}`} sx={{ width: '30px', height: '30px', backgroundColor: '#fff', boxShadow: '0 0 10px 2px rgba(0, 0, 0, .25)', borderRadius: '50%' }} display='flex' justifyContent='center' alignItems='center'>
-              <Box component='img' width='80%' src={`/${petInfo?.name}.svg`} alt={`${petInfo?.name} Icon`} />
-            </Box>
-          )
-        })}
+      <Box width='100%' display='flex' flexDirection='column'>
+        {
+          isDiscountValid ?
+            <Box width='100%' height='100%' component='img' src='/sale_stamp.png' alt='Sale Stamp' sx={{
+              borderRadius: '20px',
+            }} /> : null
+        }
+        {/* Pet Icons */}
+        <Box width='100%' display='inline-flex' flexDirection='row-reverse' gap={0.5} sx={{ position: 'absolute', bottom: 1, marginBottom: '-15px' }}>
+          {petTypes.map((petType) => {
+            const petInfo = pets.filter(pet => pet._id == petType)[0];
+            return (
+              <Box key={`${petInfo._id}-${alt}`} sx={{ width: '25px', height: '25px', backgroundColor: '#fff', boxShadow: '0 0 10px 2px rgba(0, 0, 0, .25)', borderRadius: '50%' }} display='flex' justifyContent='center' alignItems='center'>
+                <Box component='img' width='80%' src={`/${petInfo?.name}.svg`} alt={`${petInfo?.name} Icon`} />
+              </Box>
+            )
+          })}
+        </Box>
       </Box>
     </CardMedia>
   )
