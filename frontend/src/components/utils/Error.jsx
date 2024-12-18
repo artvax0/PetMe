8a441e107afa02e12258dc6b8853517c8e0f9d66
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-12-18 19:05:44 +0200

    fix error component growing

--- frontend/src/components/utils/Error.jsx
+++ frontend/src/components/utils/Error.jsx
@@ -6,10 +6,10 @@ export default function Error({ error }) {
   const { mode } = useTheme();
   return (
     <Box display='flex' justifyContent='center' alignItems='center' flexGrow={1} flexDirection='column'>
-      <Box flexGrow={1}>
+      <Box>
         <ErrorIcon sx={{ fontSize: '100px' }} color='error' />
       </Box>
-      <Box flexGrow={1}>
+      <Box>
         <Typography gutterBottom component='p' textAlign='center' fontSize={{ xs: '1.5rem', sm: '3rem' }} fontWeight='bold' color={mode == 'light' ? '#000' : '#fff'}>Something went wrong</Typography>
         <Typography gutterBottom component='p' textAlign='center' fontSize={{ xs: '1.25rem', sm: '1.5rem' }} color={mode == 'light' ? '#000' : '#fff'}>There was a problem processing the request.</Typography>
         <Typography gutterBottom component='p' textAlign='center' fontSize={{ xs: '1rem', sm: '1.25rem' }} color={mode == 'light' ? '#000' : '#fff'}>Error: {error}</Typography>
