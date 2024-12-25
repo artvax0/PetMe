cff91684aecd23d3b7cf341d7cf10c067abfbfd9
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-12-25 20:12:54 +0200

    add login page picture

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from '../components/utils/Title'
 import LoginForm from '../components/forms/LoginForm'
-import { Grid2, Typography } from '@mui/material'
+import { Box, Grid2, Typography } from '@mui/material'
 import useForm from '../hooks/useForm';
 import { initialLoginForm } from '../helpers/initial_forms/initialLoginForm';
 import useUsers from '../hooks/useUsers';
@@ -22,18 +22,25 @@ export default function LoginPage() {
     <>
       <Title title={'Login'} />
       <Grid2 container flexDirection='column' size={12} color={mode == 'light' ? '#000' : '#fff'}>
-        <Typography variant='h3' component='h1' color={mode == 'light' ? 'highlight' : '#fff'} textAlign='center' sx={{ maxWidth: { xs: '100%', sm: '50%' } }}>Welcome to PetMe!</Typography>
-        <Grid2 size={6} container flexDirection='column' justifyContent='center' flexGrow={1}>
-          <Typography variant='body1' component='p' sx={{ maxWidth: { xs: '100%', sm: '50%' } }}>Please enter your login credentials</Typography>
-          <LoginForm
-            onSubmit={onSubmit}
-            validateForm={validateForm}
-            errors={errors}
-            formData={formData}
-            onInputChange={handleChange}
-          />
+
+        <Grid2 size={12} container flexDirection={{ xs: 'column-reverse', md: 'row' }} justifyContent='center' alignItems='center' flexGrow={1}>
+          <Grid2 size={{ xs: 12, md: 6 }}>
+            <Typography variant='h3' component='h1' color={mode == 'light' ? 'highlight' : '#fff'} textAlign='center' sx={{ maxWidth: '100%' }}>Welcome to PetMe!</Typography>
+            <Typography variant='body1' component='p' sx={{ maxWidth: { xs: '100%', sm: '50%' } }}>Please enter your login credentials</Typography>
+            <LoginForm
+              onSubmit={onSubmit}
+              validateForm={validateForm}
+              errors={errors}
+              formData={formData}
+              onInputChange={handleChange}
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 6 }} container flexGrow={1} justifyContent='center' alignItems='center'>
+            <Box component='img' src='/pixellogin.svg' alt='Log in picture' maxWidth='100%' />
+          </Grid2>
         </Grid2>
-      </Grid2>
+
+      </Grid2 >
     </>
   )
 }
