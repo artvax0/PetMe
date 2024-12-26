74e11fc474e8caf1a988f01572f73e485f0fe61c
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-12-26 21:54:21 +0200

    fix card content font

--- frontend/src/components/cards/CardComponent.jsx
+++ frontend/src/components/cards/CardComponent.jsx
@@ -22,13 +22,13 @@ export default function CardComponent({ product, pets }) {
           </Box>
           <Box display='flex' flexDirection='column' alignItems='center' width='100%'>
             <CardContent sx={{ p: 1 }}>
-              <Typography gutterBottom variant="h5" fontWeight={500} component='p' sx={{ textAlign: 'center' }} fontSize={{ xs: '0.75rem', lg: '1rem', xl: '1.5rem' }}>{product.name}</Typography>
+              <Typography gutterBottom variant="h5" fontWeight={500} component='p' sx={{ textAlign: 'center' }} fontSize='0.75rem' >{product.name}</Typography>
               {isDiscountValid ?
                 <Box display='flex' gap={1} justifyContent='center'>
-                  <Typography variant='h6' fontWeight='light' component='p' sx={{ textAlign: 'center', textDecoration: 'line-through' }} fontSize={{ xs: '0.75rem', lg: '1rem', xl: '1.5rem' }} color='darkgrey' >${product.price}</Typography>
-                  <Typography variant='h6' fontWeight='medium' component='p' sx={{ textAlign: 'center' }} color='error'>${product.price * (1 - product.discount / 100)}</Typography>
+                  <Typography variant='h6' fontWeight='light' component='p' sx={{ textAlign: 'center', textDecoration: 'line-through' }} fontSize='0.75rem' color='darkgrey' >${product.price}</Typography>
+                  <Typography variant='h6' fontWeight='medium' component='p' sx={{ textAlign: 'center' }} fontSize='1.25rem' color='error'>${product.price * (1 - product.discount / 100)}</Typography>
                 </Box> :
-                <Typography variant='h6' fontWeight='medium' component='p' sx={{ textAlign: 'center' }}>${product.price}</Typography>}
+                <Typography variant='h6' fontWeight='medium' component='p' sx={{ textAlign: 'center' }} fontSize='1.25rem'>${product.price}</Typography>}
             </CardContent>
 
             {
